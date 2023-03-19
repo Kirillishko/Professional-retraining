54c8f850aaed44bff94bdb1ee66703894cb4ce9d
Kirillishko <shumaev.kirill@gmail.com>
2023-03-20 02:05:20 +0500

    20

---
+++ src/slider.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let slides = document.querySelectorAll('.gallery-wrapper-item');
+    let currentSlide = 0;
+    let slideInterval = setInterval(nextSlide, 10000);
+
+    slides[0].classList.add('active');
+
+    function nextSlide() {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
+
+    function prevSlide() {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
+
+    document.querySelector('.gallery-wrapper-controls-next').addEventListener('click', nextSlide);
+    document.querySelector('.gallery-wrapper-controls-prev').addEventListener('click', prevSlide);
+});
