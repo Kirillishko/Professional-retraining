636d3f2c078fb5adc30d2d1c10f1a260c35c82a2
Kirillishko <97043155+Kirillishko@users.noreply.github.com>
2023-03-20 15:21:51 +0500

    21

--- src/slider.js
+++ src/slider.js
@@ -9,12 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
         slides[currentSlide].classList.remove('active');
         currentSlide = (currentSlide + 1) % slides.length;
         slides[currentSlide].classList.add('active');
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, 10000);
     }
 
     function prevSlide() {
         slides[currentSlide].classList.remove('active');
         currentSlide = (currentSlide - 1 + slides.length) % slides.length;
         slides[currentSlide].classList.add('active');
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, 10000);
     }
 
     document.querySelector('.gallery-wrapper-controls-next').addEventListener('click', nextSlide);
