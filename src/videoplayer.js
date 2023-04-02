80809593d488a1f7e4df27168e69b7d62b73b3cc
Kirillishko <shumaev.kirill@gmail.com>
2023-04-02 15:10:24 +0500

    30

---
+++ src/videoplayer.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function sliderTrack() {
+        const slider = document.querySelector('.controls-progress input[type="range"]');
+        const value = slider.value;
+        slider.style.
+    }
+})
