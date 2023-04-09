e7e5f7426024b6d61619dc1f6aeb682d21ae2427
Kirillishko <shumaev.kirill@gmail.com>
2023-04-10 02:49:59 +0500

    38

--- src/videoplayer.js
+++ src/videoplayer.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         time = document.querySelector('.controls-time'),
         screen = document.querySelector('.controls-screen'),
         soundButton = document.querySelector('.controls-sound-buttons'),
-        soundProgress = document.querySelector('.controls-sound-progress'),
+        soundProgress = document.querySelector('.controls-sound-wrap-range'),
         videoButtons = [...document.querySelectorAll('.big-image-wrapper-button'), ...document.querySelectorAll('.small-image-wrapper-button')];
 
     let isVideoPlaying = false;
@@ -79,12 +79,24 @@ document.addEventListener("DOMContentLoaded", function() {
     function onProgressInput() {
         video.removeEventListener('timeupdate', setTime);
         video.pause();
-        video.currentTime = (progress.value * video.duration) / progressMax;
+        const currentTime = (progress.value * video.duration) / progressMax;
+        video.currentTime = currentTime;
+
+        const allTime = video.duration;
+        progress.value = (currentTime / allTime) * progressMax;
+
+        const currentMinutes = Math.floor(currentTime / 60);
+        const allMinutes = Math.floor(allTime / 60);
+        const currentSeconds = formatTime(Math.floor(currentTime % 60));
+        const allSeconds = formatTime(Math.floor(allTime % 60));
+
+        time.innerHTML = `${currentMinutes}:${currentSeconds} / ${allMinutes}:${allSeconds}`;
     }
 
     function toggleFullScreen() {
         if (document.fullscreenElement == null) {
             video.requestFullscreen();
+            // controls.classList.add('fullscreen');
         } else {
             document.exitFullscreen();
         }
@@ -93,6 +105,16 @@ document.addEventListener("DOMContentLoaded", function() {
     function toggleMute() {
         video.muted = !video.muted;
     }
+
+    function moveSoundSlider() {
+        let currentValue = soundProgress.value * soundProgress.max * 100;
+        soundProgress.style.background =
+            `linear-gradient(to right,
+            #4B7EFE 0%, #4B7EFE ` + currentValue + `%,
+            #0038C3 ` + currentValue + `%, #0038C3 100%)`;
+    }
+
+    moveSoundSlider();
 
     soundProgress.addEventListener('input', e => {
         video.volume = e.target.value;
@@ -131,6 +153,18 @@ document.addEventListener("DOMContentLoaded", function() {
     video.addEventListener('loadedmetadata', showVideo);
     screen.addEventListener('click', toggleFullScreen);
     progress.addEventListener('input', onProgressInput);
+    soundProgress.addEventListener('input', moveSoundSlider);
+    soundButton.addEventListener('mouseover', () => soundProgress.classList.add('active'));
+    soundButton.addEventListener('mouseout', (event) => {
+        console.log(event.relatedTarget);
+        if (event.relatedTarget !== soundProgress)
+            soundProgress.classList.remove('active')
+    });
+    soundProgress.addEventListener('mouseout', (event) => {
+        console.log(event.relatedTarget);
+        if (event.relatedTarget !== soundButton)
+            soundProgress.classList.remove('active')
+    });
 
     let clickCount = 0;
 
@@ -150,4 +184,18 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 200);
     };
 
+    // var timeout;
+    // document.querySelector('.wrap').addEventListener('focusin mouseover mousedown hover', function() {
+    //     window.clearTimeout(timeout);
+    //     document.querySelector('.wrap').classList.add('hover');
+    // });
+    // document.querySelector('.wrap').addEventListener('focusout mouseout mouseup', function() {
+    //     window.clearTimeout(timeout);
+    //     timeout = setTimeout(function(){removeHoverClass();}, 1000);
+    // });
+    // function removeHoverClass() {
+    //     if (!document.querySelector('.wrap').is(":hover")) {
+    //         document.querySelector('.wrap').classList.remove('hover');
+    //     }
+    // }
 })
